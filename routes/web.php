782b0f620654ff390b8ab88ec03e976b0eaa8cca
Yusufa Haidar <haidardewa@gmail.com>
2024-02-22 14:18:53 +0700

    Langkah Praktikum - Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/halo', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return '2141762038 (Yusufa Haidar)';
+});
