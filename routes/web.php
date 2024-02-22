f965adef08c175e3d4cbc9b9ac716e8a771850bd
Yusufa Haidar <haidardewa@gmail.com>
2024-02-22 16:35:28 +0700

    Langkah Praktikum - Membuat View

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function(){
+    return view('hello', ['name' => 'Yusufa Haidar']);
+});
